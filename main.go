44e887695db7251dd65520b65de936ce48150843
Yerdaulet Batalov <ye.batalov@kazdream.kz>
2022-12-02 20:17:33 +0600

    afpacket

--- main.go
+++ main.go
@@ -2,14 +2,78 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+	"sync"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/afpacket"
+	"github.com/google/gopacket/layers"
 )
 
 func main() {
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "hello world!")
-	})
+	fmt.Println("vim-go")
 
-	log.Fatal(http.ListenAndServeTLS(":9000", "localhost.crt", "localhost.key", nil))
+	var wg sync.WaitGroup
+
+	frameSize := afpacket.OptFrameSize(1 << 11)
+	blockSize := afpacket.OptBlockSize(frameSize << 11)
+
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			tpkt, err := afpacket.NewTPacket(
+				afpacket.OptInterface("wlp4s0"),
+				frameSize,
+				blockSize,
+				afpacket.OptNumBlocks(64),
+			)
+			if err != nil {
+				panic(err)
+			}
+
+			if err := tpkt.SetFanout(afpacket.FanoutHashWithDefrag, 1984); err != nil {
+				panic(err)
+			}
+
+			var eth layers.Ethernet
+			var ipv4 layers.IPv4
+			var ipv6 layers.IPv6
+			var tcp layers.TCP
+			var udp layers.UDP
+
+			parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ipv4, &ipv6, &tcp, &udp)
+			parser.IgnoreUnsupported = true
+			decoded := make([]gopacket.LayerType, 10)
+
+			for {
+				pkt, ci, err := tpkt.ZeroCopyReadPacketData()
+				if err != nil {
+					panic(err)
+				}
+
+				if err := parser.DecodeLayers(pkt, &decoded); err != nil {
+					fmt.Println("error decoding layers:", err)
+					continue
+				}
+
+				fmt.Println(i, ci)
+
+				for _, ltyp := range decoded {
+					switch ltyp {
+					case layers.LayerTypeIPv4:
+						fmt.Println("-- IPv4:", ipv4.SrcIP, ipv4.DstIP)
+					case layers.LayerTypeIPv6:
+						fmt.Println("-- IPv6:", ipv6.SrcIP, ipv6.DstIP)
+					case layers.LayerTypeTCP:
+						fmt.Println("-- TCP:", tcp.SrcPort, tcp.DstPort)
+					case layers.LayerTypeUDP:
+						fmt.Println("-- UDP:", udp.SrcPort, udp.DstPort)
+					}
+				}
+
+			}
+		}(i)
+	}
+
+	wg.Wait()
 }
