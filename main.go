a7abd1eb0b4f2a5660236c67910e0578cd81b0e5
Yerdaulet Batalov <ye.batalov@kazdream.kz>
2022-12-20 19:48:52 +0600

    new branch

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
 
+func main() {
+	resolver := &net.Resolver{
+		PreferGo: true,
+		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+			dialer := net.Dialer{Timeout: time.Duration(10 * time.Second)}
+			return dialer.DialContext(ctx, "udp", "8.8.8.8:53")
+		},
+	}
+	started := time.Now()
+	resolver.LookupAddr(context.Background(), os.Args[1])
+	fmt.Printf("Query took: %s", time.Since(started))
 }
