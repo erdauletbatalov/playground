a5f81fde77e495b0e2b4e6e3f502b10d30c867fe
Yerdaulet Batalov <ye.batalov@kazdream.kz>
2023-02-02 19:31:13 +0600

    wrong method

--- main.go
+++ main.go
@@ -1,56 +1,38 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
-	"time"
-
-	"github.com/miekg/dns"
+	"io"
+	"io/ioutil"
 )
 
 func main() {
-	// Set the DNS server to use for the request
-	dnsServer := "8.8.8.8:53"
+	payload := []byte("hello high value software engineer")
+	hashAndBroadcast(bytes.NewReader(payload))
+}
 
-	// Read the array of IP addresses
-	ips := []string{"216.58.211.3", "200.140.59.10", "188.235.48.62"}
-
-	// Create a channel to receive the DNS responses
-	responses := make(chan *dns.Msg)
-
-	// Send a DNS PTR request for each IP address
-	for _, ip := range ips {
-		// Create a DNS PTR record with the IP address
-		question := new(dns.Msg)
-		question.SetQuestion(ip+".in-addr.arpa.", dns.TypePTR)
-
-		// Send the DNS request in a goroutine
-		go func() {
-			c := new(dns.Client)
-			msg, _, err := c.Exchange(question, dnsServer)
-			if err != nil {
-				fmt.Printf("Error sending DNS request: %s\n", err)
-				return
-			}
-			responses <- msg
-		}()
+func hashAndBroadcast(r io.Reader) error {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
 	}
 
-	// Wait for all responses to arrive
-	for i := 0; i < len(ips); i++ {
-		select {
-		case msg := <-responses:
-			// Print the response from the DNS server
-			for _, rr := range msg.Answer {
-				if rr.Header().Rrtype == dns.TypePTR {
-					// This is a DNS PTR record, so print the PTR query and TTL value
-					fmt.Printf("PTR query: %s\n", rr.(*dns.PTR).Ptr)
-					fmt.Printf("TTL of DNS PTR record: %d\n", rr.Header().Ttl)
-				}
-			}
+	hash := sha1.Sum(b)
+	fmt.Println(hex.EncodeToString(hash[:]))
 
-		case <-time.After(5 * time.Second):
-			// Timeout after 5 seconds
-			fmt.Println("Timeout waiting for DNS response")
-		}
+	return broadcast(r)
+}
+
+func broadcast(r io.Reader) error {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
 	}
+
+	fmt.Println("string of the bytes: ", string(b))
+
+	return nil
 }
