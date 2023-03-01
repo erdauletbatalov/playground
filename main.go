0d5a225d625f67f63209746457917d3f2fd80e23
Yerdaulet Batalov <ye.batalov@kazdream.kz>
2023-03-01 19:06:08 +0600

    yml file added

--- main.go
+++ main.go
@@ -1,33 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func recordMetrics() {
-	go func() {
-		for {
-			opsProcessed.Inc()
-			time.Sleep(2 * time.Second)
-		}
-	}()
-}
-
-var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "myapp_processed_ops_total",
-		Help: "The total number of processed events",
-	})
+var pingCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "ping_request_count",
+		Help: "No of request handled by Ping handler",
+	},
 )
 
-func main() {
-	recordMetrics()
+func ping(w http.ResponseWriter, req *http.Request) {
+	pingCounter.Inc()
+	fmt.Fprintf(w, "pong")
+}
 
+func main() {
+	prometheus.MustRegister(pingCounter)
+
+	http.HandleFunc("/ping", ping)
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":2112", nil)
+	http.ListenAndServe(":8090", nil)
 }
