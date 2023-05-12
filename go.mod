3bc9807785866a865f9fa1636d6711c409eca9b4
Yerdaulet Batalov <ye.batalov@kazdream.kz>
2023-05-12 14:15:35 +0600

    Table

--- go.mod
+++ go.mod
@@ -2,15 +2,10 @@ module playground
 
 go 1.20
 
+require github.com/jedib0t/go-pretty/v6 v6.4.6
+
 require (
-	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/prometheus/client_golang v1.14.0 // indirect
-	github.com/prometheus/client_model v0.3.0 // indirect
-	github.com/prometheus/common v0.37.0 // indirect
-	github.com/prometheus/procfs v0.8.0 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,47 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"playground/metrics"
-	"time"
+	"os"
 
-	"github.com/prometheus/client_golang/prometheus"
-)
-
-var pingCounter = prometheus.NewCounter(
-	prometheus.CounterOpts{
-		Name: "ping_request_count",
-		Help: "No of request handled by Ping handler",
-	},
-)
-
-func ping(w http.ResponseWriter, req *http.Request) {
-	pingCounter.Inc()
-	fmt.Fprintf(w, "pong")
-}
-
-var (
-	ethdevXstatsLabels     = []string{"port_id", "port_name", "prefix", "xstat_name"}
-	ethdevXstatsMetricName = "ethdev_xstats"
+	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 func main() {
-	reg := prometheus.DefaultRegisterer
-	namespace := "example"
-
-	vec := metrics.NewCounterVec(reg, prometheus.CounterOpts{
-		Name:      ethdevXstatsMetricName,
-		Help:      "Help text for " + ethdevXstatsMetricName,
-		Namespace: namespace,
-	}, ethdevXstatsLabels)
-
-	portId := "1"
-	portName := "eth0"
-	prefix := "dpdk"
-	statName := "example"
-	for i := 0; i < 100; i++ {
-		time.Sleep(1 * time.Second)
-		vec.WithLabelValues(portId, portName, prefix, statName).Add(float64(i))
-	}
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"#", "First Name", "Last Name", "Salary"})
+	t.AppendRows([]table.Row{
+		{1, "Darth", "Vader", 100000},
+		{2, "Leia", "Organa", 200000},
+		{3, "Luke", "Skywalker", 300000},
+	})
+	t.Render()
 }
