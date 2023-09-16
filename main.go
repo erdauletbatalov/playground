5f90803658dd8af82b02051ffe3d921fc826a12b
Yerdaulet <ye.batalov>
2023-09-16 20:00:14 +0600

    wip
    Signed-off-by: Yerdaulet <ye.batalov>

--- main.go
+++ main.go
@@ -3,21 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(mergeAlternately("abcd", "pq"))
+	fmt.Println(gcdOfStrings())
 }
 
-func mergeAlternately(word1 string, word2 string) string {
-	var result []rune
-	if len(word1) >= len(word2) {
-		for i, val := range word2 {
-			result = append(result, rune(word1[i]), val)
-		}
-		return string(result) + word1[len(word2):]
-	} else if len(word1) <= len(word2) {
-		for i, val := range word1 {
-			result = append(result, val, rune(word2[i]))
-		}
-		return string(result) + word2[len(word1):]
-	}
-	return string(result)
+func gcdOfStrings(str1 string, str2 string) string {
+	return ""
 }
