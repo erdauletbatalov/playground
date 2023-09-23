b113d21a5316f344521a0db54a5fafa8070589d6
Yerdaulet <ye.batalov>
2023-09-23 15:38:31 +0600

    done, but slow and not efficient
    Signed-off-by: Yerdaulet <ye.batalov>

--- main.go
+++ main.go
@@ -1,49 +1,66 @@
+// Given a string s, reverse only all the vowels in the string and return it.
+
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+// Example 1:
+
+// Input: s = "hello"
+// Output: "holle"
+// Example 2:
+
+// Input: s = "leetcode"
+// Output: "leotcede"
+
+// Constraints:
+
+// 1 <= s.length <= 3 * 105
+// s consist of printable ASCII characters.
+
 package main
 
 import "fmt"
 
-func main() {
-	fmt.Println(canPlaceFlowers([]int{0}, 2))
+// initialize map
+var vowelsMap = map[rune]bool{
+	'a': true, 'e': true, 'i': true, 'o': true, 'u': true,
+	'A': true, 'E': true, 'I': true, 'O': true, 'U': true,
 }
 
-func canPlaceFlowers(flowerbed []int, n int) bool {
-	for i := range flowerbed {
-		if canPlaceFlower(flowerbed, i) {
-			n--
-			if n == 0 {
-				return true
-			}
+func main() {
+	// input string from user stdin
+	var input string
+	fmt.Scan(&input)
+
+	fmt.Println(reverseVowels(input))
+}
+
+func reverseVowels(s string) string {
+	var result []rune
+	r := findVowels(s)
+	for i := range s {
+		if isVowel(rune(s[i])) {
+			result = append(result, rune(r[len(r)-1]))
+			r = r[:len(r)-1]
+			continue
 		}
+		result = append(result, rune(s[i]))
+	}
+	return string(result)
+}
+
+func findVowels(s string) []rune {
+	vowels := make([]rune, 0)
+	for i := range s {
+		if isVowel(rune(s[i])) {
+			vowels = append(vowels, rune(s[i]))
+		}
+	}
+	return vowels
+}
+
+func isVowel(r rune) bool {
+	if _, ok := vowelsMap[r]; ok {
+		return true
 	}
 	return false
-}
-
-func canPlaceFlower(flowerbed []int, index int) bool {
-	if len(flowerbed) == 1 {
-		if flowerbed[index] == 0 {
-			return true
-		} else {
-			return false
-		}
-	}
-	switch index {
-	case 0:
-		if flowerbed[index] == 0 && flowerbed[index+1] == 0 {
-			return true
-		} else {
-			return false
-		}
-	case len(flowerbed) - 1:
-		if flowerbed[index] == 0 && flowerbed[index-1] == 0 {
-			return true
-		} else {
-			return false
-		}
-	default:
-		if flowerbed[index] == 0 && flowerbed[index-1] == 0 && flowerbed[index+1] == 0 {
-			return true
-		} else {
-			return false
-		}
-	}
 }
