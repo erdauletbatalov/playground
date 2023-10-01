d9b1b53cfcd918471bb6a91b3639c6199e005717
Yerdauelt <erdauletbatalov@gmail.com>
2023-10-01 23:44:49 +0600

    done, but very slow and ineficcient
    Signed-off-by: Yerdauelt <erdauletbatalov@gmail.com>

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
-// Given a string s, reverse only all the vowels in the string and return it.
-
-// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+// 334. Increasing Triplet Subsequence
+// Medium
+// Topics
+// Companies
+// Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
 
 // Example 1:
 
-// Input: s = "hello"
-// Output: "holle"
+// Input: nums = [1,2,3,4,5]
+// Output: true
+// Explanation: Any triplet where i < j < k is valid.
 // Example 2:
 
-// Input: s = "leetcode"
-// Output: "leotcede"
+// Input: nums = [5,4,3,2,1]
+// Output: false
+// Explanation: No triplet exists.
+// Example 3:
+
+// Input: nums = [2,1,5,0,4,6]
+// Output: true
+// Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
 
 // Constraints:
 
-// 1 <= s.length <= 3 * 105
-// s consist of printable ASCII characters.
+// 1 <= nums.length <= 5 * 105
+// -231 <= nums[i] <= 231 - 1
+
+// Follow up: Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?
 
 package main
 
@@ -24,42 +35,42 @@ import (
 
 func main() {
 	// input string from user stdin
-	input := []int{0, 1, 2}
-	fmt.Println(productExceptSelf(input))
+	input := []int{1, 2, 3, 4, 5}
+	fmt.Println(increasingTriplet(input))
 }
 
-func productExceptSelf(nums []int) []int {
+func increasingTriplet(nums []int) bool {
+	if len(nums) < 3 {
+		return false
+	}
+	difNums := make(map[int]bool)
 
-	var result = make([]int, 0, len(nums))
-	countOfZero := 0
+	for _, val := range nums {
+		if _, ok := difNums[val]; !ok {
+			difNums[val] = true
+		}
+	}
+	if len(difNums) < 3 {
+		return false
+	}
 	for i := range nums {
-		result = append(result, 0)
-		if nums[i] == 0 {
-			countOfZero++
+		if i+1 >= len(nums) {
+			continue
 		}
-	}
-	if countOfZero >= 2 {
-		return result
-	}
-	product := 1
-	for i := range result {
-		if nums[i] != 0 {
-			product *= nums[i]
-		}
-	}
-	if countOfZero == 1 {
-		for i := range nums {
-			if nums[i] == 0 {
-				result[i] = product
+		for j := i + 1; j < len(nums); j++ {
+			if j+1 >= len(nums) {
 				continue
 			}
-			result[i] = 0
-		}
-		return result
-	}
+			if nums[j] > nums[i] {
+				for k := j + 1; k < len(nums); k++ {
+					if nums[k] > nums[j] {
+						return true
+					}
+				}
+			}
 
-	for i := range nums {
-		result[i] = product / nums[i]
+		}
+
 	}
-	return result
+	return false
 }
