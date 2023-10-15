d06244287846fa5258f1bd574c78e3b226d440b9
Yerdaulet <erdauletbatalov@gmail.com>
2023-10-15 19:56:25 +0600

    done
    Signed-off-by: Yerdaulet <erdauletbatalov@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	moveZeroes([]int{0, 1, 0, 3, 12})
+	moveZeroes([]int{0, 0, 0, 1})
+	moveZeroes([]int{4, 2, 4, 0, 0, 3, 0, 5, 1, 0})
+}
+
+func moveZeroes(nums []int) {
+	zeroCount := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			for j := i + 1; j < len(nums); j++ {
+				if nums[j-1] == 0 {
+					zeroCount++
+				}
+				if nums[j] != 0 {
+					nums[j-zeroCount], nums[j] = nums[j], nums[j-zeroCount]
+					zeroCount--
+				}
+			}
+			break
+		}
+	}
+	fmt.Println(nums)
 }
