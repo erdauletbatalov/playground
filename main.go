7426568b8b1fc375f9a9bde0d3817602dbe79544
Yerdaulet <erdauletbatalov@gmail.com>
2023-10-21 19:28:38 +0600

    done
    Signed-off-by: Yerdaulet <erdauletbatalov@gmail.com>

--- main.go
+++ main.go
@@ -3,23 +3,57 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(isSubsequence("abc", "ahbgdc"))
-	fmt.Println(isSubsequence("axc", "ahbgdc"))
+	fmt.Println(maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}))
 }
 
-func isSubsequence(s string, t string) bool {
-	subs := 0
-	for i := 0; i < len(s); i++ {
-		for j := 0; j < len(t); j++ {
-			if s[i] == t[j] && j+1 < len(t) {
-				subs++
-				t = t[j+1:]
-				break
-			} else if s[i] == t[j] {
-				subs++
-				return subs == len(s)
-			}
+func maxArea(height []int) int {
+	var l, result int
+	r := len(height) - 1
+
+	for l < r {
+		currArea := min(height[l], height[r]) * (r - l)
+		result = max(result, currArea)
+
+		if height[l] < height[r] {
+			l++
+		} else {
+			r--
 		}
 	}
-	return subs == len(s)
+	return result
 }
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// Brute force
+// func maxArea(height []int) int {
+// 	max := 0
+// 	for i := 0; i < len(height); i++ {
+// 		for j := i + 1; j < len(height); j++ {
+// 			if height[i] >= height[j] {
+// 				area := height[j] * (j - i)
+// 				if area > max {
+// 					max = area
+// 				}
+// 			} else if height[i] < height[j] {
+// 				area := height[i] * (j - i)
+// 				if area > max {
+// 					max = area
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return max
+// }
