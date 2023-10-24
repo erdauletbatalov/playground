f80d49db687989d5ee07cb2cbd3180e4ed256623
Yerdaulet <erdauletbatalov@gmail.com>
2023-10-24 17:48:13 +0600

    livedock https://www.educative.io/answers/what-is-livelock-in-golang
    Signed-off-by: Yerdaulet <erdauletbatalov@gmail.com>

--- main.go
+++ main.go
@@ -1,43 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println(maxVowels("hellooo", 4))
-}
+	runtime.GOMAXPROCS(4)
+	type value struct {
+		sync.Mutex
+		id     string
+		locked bool
+	}
 
-func maxVowels(s string, k int) int {
-	curr := 0
-	vowels := "aeiou"
-	var max int
-	if len(s) < k {
-		return 0
+	lock := func(v *value) {
+		v.Lock()
+		v.locked = true
 	}
-	for i := 0; i < k; i++ {
-		if contains(vowels, s[i]) {
-			max++
-		}
-		curr = max
+	unlock := func(v *value) {
+		v.Unlock()
+		v.locked = false
 	}
-	for i := 1; i < len(s)-k+1; i++ {
-		if contains(vowels, s[i-1]) {
-			curr--
-		}
-		if contains(vowels, s[i+k-1]) {
-			curr++
-		}
-		if curr > max {
-			max = curr
-		}
-	}
-	return max
-}
+	move := func(wg *sync.WaitGroup, id string, v1, v2 *value) {
+		defer wg.Done()
+		for i := 0; ; i++ {
+			if i >= 3 {
+				fmt.Println("canceling goroutine...")
+				return
+			}
 
-func contains(str string, ch byte) bool {
-	for i := range str {
-		if ch == str[i] {
-			return true
+			fmt.Printf("%v: Goroutine is locking\n", v1.id)
+			lock(v1) // <1>
+
+			time.Sleep(2 * time.Second)
+
+			if v2.locked { // <2>
+				fmt.Printf("%v: Goroutine is , blocked by %v\n", v1.id, v2.id)
+				unlock(v1) // <3>
+				continue
+			}
 		}
 	}
-	return false
+	a, b, c := value{id: "Process1"}, value{id: "Process2"}, value{id: "Process3"}
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go move(&wg, "first", &a, &b)
+	go move(&wg, "second", &b, &c)
+	wg.Wait()
 }
