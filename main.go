3b38f7892895cc89e23194391b758775f20c97db
Yerdaulet <erdauletbatalov@gmail.com>
2023-11-05 19:29:30 +0600

    done
    Signed-off-by: Yerdaulet <erdauletbatalov@gmail.com>

--- main.go
+++ main.go
@@ -7,6 +7,62 @@ func main() {
 }
 
 func longestOnes(nums []int, k int) int {
+	var res, max int
+	var tempArr []int
 
-	return 0
+	var nulls int
+
+	for i := range nums {
+		if nums[i] == 1 {
+			res++
+		} else {
+			nulls++
+		}
+		if nums[i] != 1 || i == len(nums)-1 {
+			if max < res {
+				max = res
+			}
+			res = 0
+		}
+	}
+	if k == 0 {
+		return max
+	}
+	if nulls < k {
+		return len(nums)
+	}
+	res = 0
+	max = 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			k--
+			nums[i] = 1
+			tempArr = append(tempArr, i)
+			if k == 0 {
+				for j := tempArr[0]; j >= 0; j-- {
+					if nums[j] == 0 {
+						break
+					}
+					res++
+				}
+				for j := tempArr[0]; j < len(nums); j++ {
+					if nums[j] == 0 {
+						break
+					}
+					res++
+				}
+				res = res - 1
+				if max < res {
+					max = res
+				}
+				res = 0
+				nums[tempArr[0]] = 0
+				tempArr = tempArr[1:]
+				k++
+
+			}
+		}
+	}
+	return max
 }
