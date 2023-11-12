ac869f04c712727b6d17a3b8af1821068e5441b0
Yerdaulet <erdauletbatalov@gmail.com>
2023-11-12 23:46:43 +0600

    Runtime Details 60ms Beats 48.23%of users with Go Memory Details 6.57MB Beats 99.68%of users with Go
    Signed-off-by: Yerdaulet <erdauletbatalov@gmail.com>

--- main.go
+++ main.go
@@ -1,15 +1,54 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "hello world!")
-	})
+	fmt.Println(closeStrings("cabbba", "abbccc"))
+}
 
-	log.Fatal(http.ListenAndServeTLS(":9000", "localhost.crt", "localhost.key", nil))
+func closeStrings(word1 string, word2 string) bool {
+	firstMap := make(map[rune]int)
+	var firstSum int
+	secondMap := make(map[rune]int)
+	var secondSum int
+	thirdMap := make(map[int]int)
+	forthMap := make(map[int]int)
+
+	for _, val := range word1 {
+		firstMap[val]++
+	}
+	for _, val := range word2 {
+		secondMap[val]++
+	}
+	for key, val := range firstMap {
+		if _, ok := secondMap[key]; !ok {
+			return false
+		}
+		firstSum += val
+	}
+	for key, val := range secondMap {
+		if _, ok := firstMap[key]; !ok {
+			return false
+		}
+		secondSum += val
+	}
+	if firstSum != secondSum {
+		return false
+	}
+	for _, val := range firstMap {
+		thirdMap[val]++
+	}
+	for _, val := range secondMap {
+		forthMap[val]++
+	}
+	if len(firstMap) != len(secondMap) {
+		return false
+	}
+	for thirdKey, thirdVal := range thirdMap {
+		if forthVal, ok := forthMap[thirdKey]; ok && thirdVal == forthVal {
+			continue
+		}
+		return false
+	}
+	return true
 }
