b41fdc4cfa617bddfbf8c5e23f391d25f781f9ae
Yerdaulet <erdauletbatalov@gmail.com>
2023-11-19 21:45:23 +0600

    Runtime 39 ms Beats 37.08% of users with Go Memory 6.90 MB Beats 89.23% of users with Go
    Signed-off-by: Yerdaulet <erdauletbatalov@gmail.com>

--- main.go
+++ main.go
@@ -3,53 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(closeStrings("cabbba", "abbccc"))
+	fmt.Println(removeStart("st*r*"))
 }
 
-func closeStrings(word1 string, word2 string) bool {
-	firstMap := make(map[rune]int)
-	var firstSum int
-	secondMap := make(map[rune]int)
-	var secondSum int
-	thirdMap := make(map[int]int)
-	forthMap := make(map[int]int)
+func removeStart(s string) string {
+	var result []rune
+	for _, val := range s {
+		if isChar(val) {
+			result = append(result, val)
+		} else {
+			result = result[:len(result)-1]
+		}
+	}
+	return string(result)
+}
 
-	for _, val := range word1 {
-		firstMap[val]++
-	}
-	for _, val := range word2 {
-		secondMap[val]++
-	}
-	for key, val := range firstMap {
-		if _, ok := secondMap[key]; !ok {
-			return false
-		}
-		firstSum += val
-	}
-	for key, val := range secondMap {
-		if _, ok := firstMap[key]; !ok {
-			return false
-		}
-		secondSum += val
-	}
-	if firstSum != secondSum {
-		return false
-	}
-	for _, val := range firstMap {
-		thirdMap[val]++
-	}
-	for _, val := range secondMap {
-		forthMap[val]++
-	}
-	if len(firstMap) != len(secondMap) {
-		return false
-	}
-	for thirdKey, thirdVal := range thirdMap {
-		if forthVal, ok := forthMap[thirdKey]; ok && thirdVal == forthVal {
-			continue
-		}
-		return false
-
-	}
-	return true
+func isChar(s rune) bool {
+	// ASCII alphabetic characters
+	return (s >= 'a' && s <= 'z') || (s >= 'A' && s <= 'Z')
 }
