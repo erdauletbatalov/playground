5a73da0bf9759c11e8887586b13eb6b74469f2c5
yerdaulet thinkpad t480s <erdauletbatalov@gmail.com>
2024-06-14 14:34:37 +0500

    feat: add runes
    Signed-off-by: yerdaulet thinkpad t480s <erdauletbatalov@gmail.com>

--- main.go
+++ main.go
@@ -3,21 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(mergeAlternately("abcd", "pq"))
+	fmt.Println(mergeAlternately("аыфва", "pq"))
 }
 
 func mergeAlternately(word1 string, word2 string) string {
 	var result []rune
-	if len(word1) >= len(word2) {
-		for i, val := range word2 {
-			result = append(result, rune(word1[i]), val)
+	word1runes := []rune(word1)
+	word2runes := []rune(word2)
+	if len(word1runes) >= len(word2runes) {
+		for i, val := range word2runes {
+			result = append(result, word1runes[i], val)
 		}
-		return string(result) + word1[len(word2):]
-	} else if len(word1) <= len(word2) {
-		for i, val := range word1 {
-			result = append(result, val, rune(word2[i]))
+		return string(result) + string(word1runes[len(word2runes):])
+	} else {
+		for i, val := range word1runes {
+			result = append(result, val, rune(word2runes[i]))
 		}
-		return string(result) + word2[len(word1):]
+		return string(result) + string(word2runes[len(word1runes):])
 	}
-	return string(result)
 }
