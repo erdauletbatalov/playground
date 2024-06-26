f5b483d8cb7d0cc2396523a9578195fb5304279d
yerdaulet thinkpad t480s <erdauletbatalov@gmail.com>
2024-06-26 11:02:41 +0500

    ozon question
    Signed-off-by: yerdaulet thinkpad t480s <erdauletbatalov@gmail.com>

--- main.go
+++ main.go
@@ -2,46 +2,33 @@ package main
 
 import "fmt"
 
-type F interface {
-	f()
+type Foo struct {
+	a string
 }
 
-type S1 struct {
-	a int
+func (f Foo) A(s string) string {
+	return s
+}
+func (f Foo) B(s string) string {
+	return s
+}
+func (f Foo) C(s string) string {
+	return s
 }
 
-func (s S1) f() {}
+type AB interface {
+	A(s string) string
+	B(s string) string
+}
 
-type S2 struct{}
-
-func (s *S2) f() {}
+type BC interface {
+	B(s string) string
+	C(s string) string
+}
 
 func main() {
-
-	// s1 := []int{1, 2, 3}
-	// s2 := s1
-
-	s3 := struct{}{}
-	s4 := s3
-
-	s5 := S1{a: 1}
-	s6 := S2{}
-
-	fmt.Println(s5 == s6)
-	fmt.Println(s3 == s4)
-	// fmt.Println(s1 == s2)
-
-	// s1Val := S1{}
-	// s1Ptr := &S1{}
-	// s2Val := S2{}
-	// s2Ptr := &S2{}
-
-	// var i F
-	// i = s1Val
-	// i = s1Ptr
-	// i = s2Val
-	// i = s2Ptr
-
-	// fmt.Println(i)
-
+	var f AB = Foo{}
+	fmt.Println(f.B("ooh hell"))
+	newF := f.(BC)
+	fmt.Println(newF.A("naah"))
 }
